fa5a911114034ba157e45e8e557b653301d71160
Aastha-Bhatia <aasthabhatia.er@gmail.com>
2026-06-21 05:42:26 +0530

    fixed video rendering

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import Video from './Video'
 
 const HomeHeroText=()=>{
   return(
     <div className='font-[font1] pt-5 lg:mt-0 md:mt-0 mt-55 text-center'>
       <div className='lg:text-[9vw] md:text-[8vw] text-[12vw] uppercase lg:leading-[9vw] md:leading-[9vw] leading-[11vw] justify-center flex items-center'>L'étincelle</div>
       <div className='lg:text-[9vw] md:text-[8vw] text-[12vw] uppercase lg:leading-[9vw] md:leading-[9vw] leading-[11vw] justify-center flex items-center'>qui
-        <div className='w-[16vw] h-[7vw] -mt-5 rounded-full overflow-hidden'><Video/>
+        <div className='w-[16vw] h-[7vw] -mt-5 rounded-full overflow-hidden'>
+        <video 
+          src="/video.mp4"
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="w-full h-full object-cover"
+          />
         </div>
         génère
         </div>
